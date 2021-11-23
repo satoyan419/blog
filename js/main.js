5dd09b9e33d456dbdb09217b378162336b6d4114
satoyan419 <satoshi.shoji419@gmail.com>
2021-11-24 05:55:04 +0900

    レイアウトを修正

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-var menuButton = document.getElementById( 'menu-button' );
-var menuPanel = document.getElementById( 'menu-panel' );
-
-( function() {
+document.addEventListener('DOMContentLoaded', function() {
+  var menuButton = document.getElementById( 'menu-button' );
+  var menuPanel = document.getElementById( 'menu-panel' );
   menuButton.addEventListener( 'click', function( event ) {
     if ( menuButton.getAttribute( 'aria-expanded' ) === 'true' ) {
       menuButton.setAttribute( 'aria-expanded', 'false' );
@@ -11,4 +10,23 @@ var menuPanel = document.getElementById( 'menu-panel' );
       menuPanel.setAttribute( 'aria-expanded', 'true' );
     }
   } );
-}() );
+});
+
+
+const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
+for (let i = 0; i < smoothScrollTrigger.length; i++){
+  smoothScrollTrigger[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    let href = smoothScrollTrigger[i].getAttribute('href');
+      let targetElement = document.getElementById(href.replace('#', ''));
+    const rect = targetElement.getBoundingClientRect().top;
+    const offset = window.pageYOffset;
+    // const gap = 60;
+    // const target = rect + offset - gap;
+    const target = rect + offset;
+    window.scrollTo({
+      top: target,
+      behavior: 'smooth',
+    });
+  });
+}
