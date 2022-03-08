1246057ea18a78963951777f4753e272859324d0
satoyan419 <satoshi.shoji419@gmail.com>
2022-03-08 09:20:39 +0900

    JavaScriptの記述を修正

--- js/main.js
+++ js/main.js
@@ -10,40 +10,44 @@
 //   }
 // });
 
-
-const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
-for (let i = 0; i < smoothScrollTrigger.length; i++){
-  smoothScrollTrigger[i].addEventListener('click', (e) => {
-    e.preventDefault();
-    let href = smoothScrollTrigger[i].getAttribute('href');
-    let target;
-    if (href === '#top') {
-      target = 0;
-    } else {
-      let targetElement = document.getElementById(href.replace('#', ''));
-      const rect = targetElement.getBoundingClientRect().top;
-      const offset = window.pageYOffset;
-      target = rect + offset;
-    }
-    window.scrollTo({
-      top: target,
-      behavior: 'smooth',
+// smooth scroll
+{
+  const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
+  for (let i = 0; i < smoothScrollTrigger.length; i++){
+    smoothScrollTrigger[i].addEventListener('click', (e) => {
+      e.preventDefault();
+      let href = smoothScrollTrigger[i].getAttribute('href');
+      let target;
+      if (href === '#top') {
+        target = 0;
+      } else {
+        let targetElement = document.getElementById(href.replace('#', ''));
+        const rect = targetElement.getBoundingClientRect().top;
+        const offset = window.pageYOffset;
+        target = rect + offset;
+      }
+      window.scrollTo({
+        top: target,
+        behavior: 'smooth',
+      });
     });
-  });
+  }
 }
 
-
-const item = document.querySelector('.scroll-observer');
-const showButton = (items) => {
-  const button = document.getElementById('pagetop');
-  items.forEach((item) => {
-    item.isIntersecting ? button.setAttribute('data-scrolled', 'false') : button.setAttribute('data-scrolled', 'true');
-  });
+// scroll observer
+{
+  const item = document.querySelector('.scroll-observer');
+  const showButton = (items) => {
+    const button = document.getElementById('pagetop');
+    items.forEach((item) => {
+      item.isIntersecting ? button.setAttribute('data-scrolled', 'false') : button.setAttribute('data-scrolled', 'true');
+    });
+  }
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0
+  }
+  const observer = new IntersectionObserver(showButton, options);
+  observer.observe(item);
 }
-const options = {
-  root: null,
-  rootMargin: "0px",
-  threshold: 0
-}
-const observer = new IntersectionObserver(showButton, options);
-observer.observe(item);
