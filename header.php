b20a3a66792fa71d6a7c6986c9a8f9808fa439e2
satoyan419 <satoshi.shoji419@gmail.com>
2022-05-08 04:31:20 +0900

    Twitterのコンバージョントラッキングのコードを追加

--- header.php
+++ header.php
@@ -18,6 +18,16 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@500&display=swap">
   <?php wp_head(); ?>
+  <!-- Twitter universal website tag code -->
+  <script>
+  !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+  },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
+  a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+  // Insert Twitter Pixel ID and Standard Event data below
+  twq('init','o7e71');
+  twq('track','PageView');
+  </script>
+  <!-- End Twitter universal website tag code -->
 </head>
 <body>
   <?php wp_body_open(); ?>
